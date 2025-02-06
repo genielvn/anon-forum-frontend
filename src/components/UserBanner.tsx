b7cea6165012efa7573bd94e60d1a4e4c6b4fbe5
Genesis Lovino <genesislovino@gmail.com>
2025-02-06 18:14:52 +0800

    Added comments

--- src/components/UserBanner.tsx
+++ src/components/UserBanner.tsx
@@ -1,3 +1,27 @@
+/**
+ * Program Title: User Banner Component (UserBanner.tsx)
+ *
+ * Programmers: Genesis Lovino, David Andrei Estrella
+ *
+ * Purpose:
+ * - This component is used to display a banner with user information, such as username and user status.
+ *
+ * Where the program fits in the general system designs:
+ * - This file is part of the frontend of the application. It is a reusable component that can be used across different pages to display user information.
+ *
+ * Date written: April 2024
+ * Date revised: January 2025
+ *
+ * Data structures used:
+ * - UserBannerProps: Interface for the user banner component's props, containing username and status.
+ *
+ * Algorithms used:
+ * - None
+ *
+ * Control Structures:
+ * - Conditional rendering for displaying user status.
+ */
+
 import React from "react";
 import Image from "next/image";
 import style from "./UserBanner.module.scss";
